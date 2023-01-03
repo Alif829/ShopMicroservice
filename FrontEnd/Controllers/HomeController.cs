a043128e8edc65eb38d72edca9ee765be8e67541
Alif829 <bsse1120@iit.du.ac.bd>
2023-01-03 10:18:37 +0600

    Add project files.

---
+++ FrontEnd/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using FrontEnd.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace FrontEnd.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Review()
+        {
+            return View();
+        }
+       
+        public IActionResult About()
+        {
+            return View();
+        }
+        public IActionResult Fruit()
+        {
+            return View();
+        }
+        public IActionResult Contact()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
